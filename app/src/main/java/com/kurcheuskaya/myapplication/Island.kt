ae56f50415e0c4a7aeeb9c079378618df075e580
vkurcheuskaya <o.kurcheuskaya@gmail.com>
2023-01-18 20:45:29 +0800

    dfs

---
+++ app/src/main/java/com/kurcheuskaya/myapplication/Island.kt
@@ -0,0 +1,40 @@
+package com.kurcheuskaya.myapplication
+
+fun main() {
+    println(
+        numIslands(
+            arrayOf(
+                charArrayOf('1', '1', '1', '1', '0'),
+                charArrayOf('1', '1', '0', '1', '0'),
+                charArrayOf('1', '1', '0', '0', '0'),
+                charArrayOf('0', '0', '0', '0', '0')
+            )
+        )
+    )
+}
+
+fun numIslands(grid: Array<CharArray>): Int {
+    if (grid.isEmpty() || grid[0].isEmpty()) return 0
+    var count = 0
+    val row = grid.size
+    val cols = grid[0].size
+    for (i in 0 until row) {
+        for (j in 0 until cols) {
+            if (grid[i][j] == '1') {
+                count++
+                dfs(grid, i, j)
+            }
+        }
+    }
+    return count
+}
+
+fun dfs(grid: Array<CharArray>, i: Int, j: Int) {
+    if (i < 0 || i >= grid.size || j <= 0 || j >= grid[0].size || grid[i][j] == '0')
+        return
+    grid[i][j] = '0'
+    dfs(grid, i + 1, j)
+    dfs(grid, i - 1, j)
+    dfs(grid, i, j + 1)
+    dfs(grid, i, j - 1)
+}
