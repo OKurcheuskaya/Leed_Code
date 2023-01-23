24fea72cf4f33a0f307e91652cbbd9b9e3bdf16a
vkurcheuskaya <o.kurcheuskaya@gmail.com>
2023-01-23 18:43:54 +0800

    topKFrequent

--- app/src/main/java/com/kurcheuskaya/myapplication/Yandex/HeapHash.kt
+++ app/src/main/java/com/kurcheuskaya/myapplication/Yandex/HeapHash.kt
@@ -2,10 +2,16 @@ package com.kurcheuskaya.myapplication.Yandex
 
 fun main() {
     println(topKFrequent(arrayOf("i", "love", "leetcode", "i", "love", "coding"), k = 2))
+    println(topKFrequent(nums = intArrayOf(4, 1, -1, 2, -1, 2, 3), k = 2))
 }
 
 fun topKFrequent(words: Array<String>, k: Int): List<String> {
-    val wordCount= words.groupingBy { it }.eachCount()
+    val wordCount = words.groupingBy { it }.eachCount()
     val sortedWords = wordCount.toList().sortedWith(compareBy({ -it.second }, { it.first }))
     return sortedWords.take(k).map { it.first }
+}
+
+fun topKFrequent(nums: IntArray, k: Int): IntArray {
+    val new = nums.groupBy { it }.toList()
+    return new.sortedByDescending { it.second.size }.take(k).map { it.first }.toIntArray()
 }
