e9bf08d177770093163ac876fd52dc7450f4af77
vkurcheuskaya <o.kurcheuskaya@gmail.com>
2023-01-25 17:11:05 +0800

    closestMeetingNode

--- app/src/main/java/com/kurcheuskaya/myapplication/Graph.kt
+++ app/src/main/java/com/kurcheuskaya/myapplication/Graph.kt
@@ -1,12 +1,15 @@
 package com.kurcheuskaya.myapplication
 
 import java.util.*
+import kotlin.collections.HashMap
 
 
 fun main() {
     println(longestPath(intArrayOf(-1, 0, 0, 1, 1, 2), s = "abacbe"))
 //    println(findJudge(n = 2, trust = arrayOf(intArrayOf(1, 2))))
-    println(findJudge(n = 3, trust = arrayOf(intArrayOf(1,3), intArrayOf(2,3))))
+    println(findJudge(n = 3, trust = arrayOf(intArrayOf(1, 3), intArrayOf(2, 3))))
+    println(closestMeetingNode(edges = intArrayOf(2, 2, 3, -1), node1 = 0, node2 = 1))
+    println(closestMeetingNode(edges = intArrayOf(1, 2, -1), node1 = 0, node2 = 1))
 }
 
 var longValue = 1 // variable to store the length of the longest path
@@ -58,4 +61,35 @@ fun findJudge(n: Int, trust: Array<IntArray>): Int {
 
     }
     return -1
+}
+
+fun closestMeetingNode(edges: IntArray, node1: Int, node2: Int): Int {
+    val distances = mutableMapOf<Int, Int>()
+    var n = node1
+    var dist = 0
+    while (n != -1) {
+        if (distances.contains(n)) break
+        distances[n] = dist
+        n = edges[n]
+        dist++
+    }
+    n = node2
+    dist = 0
+    var min = Int.MAX_VALUE
+    var res = -1
+    while (n != -1) {
+        if (distances.contains(n)) {
+            val one = distances[n]!!
+            val max = maxOf(one, dist)
+            if (max < min || max == min && n < res) {
+                min = max
+                res = n
+            }
+        }
+        val tmp = edges[n]
+        edges[n] = -1
+        n = tmp
+        dist++
+    }
+    return res
 }
