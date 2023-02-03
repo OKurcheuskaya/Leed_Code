8f1fcd80b0740f700d290f53cba18b5a1542dd1f
vkurcheuskaya <o.kurcheuskaya@gmail.com>
2023-02-03 13:07:04 +0800

    maxProfitSameDay

--- app/src/main/java/com/kurcheuskaya/myapplication/Yandex/GreedyProblems.kt
+++ app/src/main/java/com/kurcheuskaya/myapplication/Yandex/GreedyProblems.kt
@@ -2,6 +2,7 @@ package com.kurcheuskaya.myapplication.Yandex
 
 fun main() {
     println(maxProfit(prices = intArrayOf(7, 1, 5, 3, 6, 4)))
+    println(maxProfitSameDay(prices = intArrayOf(7, 1, 5, 3, 6, 4)))
 }
 
 fun maxProfit(prices: IntArray): Int {
@@ -14,6 +15,17 @@ fun maxProfit(prices: IntArray): Int {
             maxProfit = maxOf(maxProfit, price - min)
         }
 
+    }
+    return maxProfit
+}
+
+//[7,1,5,3,6,4]
+fun maxProfitSameDay(prices: IntArray): Int {
+    var maxProfit = 0
+    for (i in 1 until prices.size) {
+        if (prices[i] > prices[i - 1]) {
+            maxProfit += prices[i]-prices[i-1]
+        }
     }
     return maxProfit
 }
