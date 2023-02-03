1e3f1265ff09b333358dc1a8303a8ca21ca2fadd
vkurcheuskaya <o.kurcheuskaya@gmail.com>
2023-02-03 12:35:15 +0800

    bubbleSort

--- app/src/main/java/com/kurcheuskaya/myapplication/Yandex/Sort.kt
+++ app/src/main/java/com/kurcheuskaya/myapplication/Yandex/Sort.kt
@@ -21,6 +21,17 @@ fun main() {
         )
     )
 }
+fun bubbleSort(arr: IntArray) {
+    for (i in 0 until arr.size - 1) {
+        for (j in 0 until arr.size - i - 1) {
+            if (arr[j] > arr[j + 1]) {
+                val temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+            }
+        }
+    }
+}
 
 fun merge(intervals: Array<IntArray>): Array<IntArray> {
     if (intervals.isEmpty()) return arrayOf()
