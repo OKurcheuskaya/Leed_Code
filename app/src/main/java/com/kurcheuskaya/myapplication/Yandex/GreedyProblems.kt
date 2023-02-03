7423bea19d9113b89cd90728fa18791a123f3246
vkurcheuskaya <o.kurcheuskaya@gmail.com>
2023-02-03 13:18:52 +0800

    maxProfitFee

--- app/src/main/java/com/kurcheuskaya/myapplication/Yandex/GreedyProblems.kt
+++ app/src/main/java/com/kurcheuskaya/myapplication/Yandex/GreedyProblems.kt
@@ -3,6 +3,7 @@ package com.kurcheuskaya.myapplication.Yandex
 fun main() {
     println(maxProfit(prices = intArrayOf(7, 1, 5, 3, 6, 4)))
     println(maxProfitSameDay(prices = intArrayOf(7, 1, 5, 3, 6, 4)))
+    println(maxProfitFee( prices = intArrayOf(1,3,2,8,4,9), fee = 2))
 }
 
 fun maxProfit(prices: IntArray): Int {
@@ -24,8 +25,18 @@ fun maxProfitSameDay(prices: IntArray): Int {
     var maxProfit = 0
     for (i in 1 until prices.size) {
         if (prices[i] > prices[i - 1]) {
-            maxProfit += prices[i]-prices[i-1]
+            maxProfit += prices[i] - prices[i - 1]
         }
     }
     return maxProfit
+}
+
+fun maxProfitFee(prices: IntArray, fee: Int): Int {
+    var cash = 0
+    var hold = -prices[0]
+    for (i in 1 until prices.size) {
+        cash = maxOf(cash, hold + prices[i] - fee)
+        hold = maxOf(cash - prices[i], hold)
+    }
+    return cash
 }
