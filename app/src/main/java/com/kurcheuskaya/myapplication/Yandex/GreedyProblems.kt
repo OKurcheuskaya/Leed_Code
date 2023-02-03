bc38529f349b2ab0a58ffecca0f7cf6b75859919
vkurcheuskaya <o.kurcheuskaya@gmail.com>
2023-02-03 13:26:14 +0800

    maxProfitDp

--- app/src/main/java/com/kurcheuskaya/myapplication/Yandex/GreedyProblems.kt
+++ app/src/main/java/com/kurcheuskaya/myapplication/Yandex/GreedyProblems.kt
@@ -1,5 +1,7 @@
 package com.kurcheuskaya.myapplication.Yandex
 
+import kotlin.math.max
+
 fun main() {
     println(maxProfit(prices = intArrayOf(7, 1, 5, 3, 6, 4)))
     println(maxProfitSameDay(prices = intArrayOf(7, 1, 5, 3, 6, 4)))
@@ -39,4 +41,20 @@ fun maxProfitFee(prices: IntArray, fee: Int): Int {
         hold = maxOf(cash - prices[i], hold)
     }
     return cash
+}
+
+fun maxProfitDp(prices: IntArray): Int {
+    val n = prices.size
+    var cool = 0
+    var buy = -prices[0]
+    var sell = 0
+    for (i in 1 until n) {
+        val coolN = maxOf(cool, sell)
+        val buyN = maxOf(buy, cool - prices[i])
+        val sellN = prices[i] + buy
+        cool = coolN
+        buy = buyN
+        sell = sellN
+    }
+    return maxOf(cool, maxOf(buy, sell))
 }
