285a947d0be871c4a4292b009438d7abf3c910c9
vkurcheuskaya <o.kurcheuskaya@gmail.com>
2023-02-03 12:35:57 +0800

    quickSort

--- app/src/main/java/com/kurcheuskaya/myapplication/Yandex/Sort.kt
+++ app/src/main/java/com/kurcheuskaya/myapplication/Yandex/Sort.kt
@@ -51,3 +51,29 @@ fun merge(intervals: Array<IntArray>): Array<IntArray> {
     return result.toTypedArray()
 
 }
+fun quickSort(arr: IntArray, low: Int, high: Int) {
+    if (low < high) {
+        val pivotIndex = partition(arr, low, high)
+        quickSort(arr, low, pivotIndex - 1)
+        quickSort(arr, pivotIndex + 1, high)
+    }
+}
+
+fun partition(arr: IntArray, low: Int, high: Int): Int {
+    val pivot = arr[high]
+    var i = low - 1
+    for (j in low until high) {
+        if (arr[j] <= pivot) {
+            i++
+            swap(arr, i, j)
+        }
+    }
+    swap(arr, i + 1, high)
+    return i + 1
+}
+
+fun swap(arr: IntArray, i: Int, j: Int) {
+    val temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
+}
