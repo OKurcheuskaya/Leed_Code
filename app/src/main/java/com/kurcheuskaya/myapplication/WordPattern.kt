86c140a4be1af04b1c8b3168a5ef7feb557edccb
vkurcheuskaya <o.kurcheuskaya@gmail.com>
2023-02-05 19:55:35 +0800

    checkInclusion

--- app/src/main/java/com/kurcheuskaya/myapplication/WordPattern.kt
+++ app/src/main/java/com/kurcheuskaya/myapplication/WordPattern.kt
@@ -4,13 +4,18 @@ fun main() {
     println(wordPattern(pattern = "abba", s = "dog cat cat dog"))
     println(gcdOfStrings(str1 = "ABCABCABC", str2 = "ABCABC"))
     println(zigzagConvert(s = "PAYPALISHIRING", numRows = 3))
+    println(checkInclusion(s1 = "ab", s2 = "eidbaooo"))
     println(
         findAllConcatenatedWordsInADict(
             words = arrayOf(
-                "a", "b","ab", "abc"
+                "a", "b", "ab", "abc"
             )
         )
     )
+    val u= "s"
+    val k= "s"
+    println(u==k)
+    println(solution(intArrayOf(1, 3, 6, 4, 1, 2)))
 }
 
 fun wordPattern(pattern: String, s: String): Boolean {
@@ -61,6 +66,7 @@ fun findAllConcatenatedWordsInADict(words: Array<String>): List<String> {
         result
     }
 }
+
 fun gcdOfStrings(str1: String, str2: String): String {
     if (str1 + str2 != str2 + str1) return ""
     val gcdVal = gcd(str1.length, str2.length)
@@ -101,19 +107,19 @@ private fun Set<String>.isConcatenatedWord(
     word: String,
     cache: MutableMap<String, Boolean>
 ): Boolean {
-    if(word in this) {
+    if (word in this) {
         return true
     }
 
-    if(word in cache) {
+    if (word in cache) {
         return cache[word]!!
     }
 
-    for(i in word.indices) {
-        val left = word.substring(0, i+1)
-        val right = word.substring(i+1)
+    for (i in word.indices) {
+        val left = word.substring(0, i + 1)
+        val right = word.substring(i + 1)
 
-        if(left in this && isConcatenatedWord(right, cache)) {
+        if (left in this && isConcatenatedWord(right, cache)) {
             cache[word] = true
             return true
         }
@@ -121,4 +127,37 @@ private fun Set<String>.isConcatenatedWord(
 
     cache[word] = false
     return false
+}
+
+fun solution(A: IntArray): Int {
+    val new = A.sortedBy { it }.toHashSet().toList()
+    for (i in new.indices) {
+        if (new[1] > 0 && i + 1 < new.size) {
+            if (new[i + 1] != new[i] + 1) return new[i] + 1
+        } else {
+            if (new.last() > 0) return new.last() + 1
+        }
+    }
+    return 1
+}
+fun checkInclusion(s1: String, s2: String): Boolean {
+    if (s1.length > s2.length) return false
+
+    val s1map = IntArray(26)
+    for (c in s1.toCharArray()) s1map[c - 'a']++
+
+    for (i in 0 until s2.length - s1.length + 1) {
+        val s2map = IntArray(26)
+        for (j in 0 until s1.length) {
+            val c = s2[i + j]
+            s2map[c - 'a']++
+        }
+        if (matches(s1map, s2map)) return true
+    }
+    return false
+}
+
+fun matches(s1map: IntArray, s2map: IntArray): Boolean {
+    for (i in 0 until 26) if (s1map[i] != s2map[i]) return false
+    return true
 }
