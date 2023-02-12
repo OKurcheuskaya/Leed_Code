c72331b2f7447a1a142ccab5ee8ea8f4b133fc8f
vkurcheuskaya <o.kurcheuskaya@gmail.com>
2023-02-12 20:48:51 +0800

    minimumFuelCost, countSubTrees, minTime, stackPush

--- app/src/main/java/com/kurcheuskaya/myapplication/TreeNode.kt
+++ app/src/main/java/com/kurcheuskaya/myapplication/TreeNode.kt
@@ -2,15 +2,13 @@ package com.kurcheuskaya.myapplication
 
 import java.util.*
 
-fun main(){
+fun main() {
     val first = TreeNode(1)
     first.left = TreeNode(2)
     first.right = TreeNode(3)
     val second = TreeNode(1)
     second.left = TreeNode(2)
     second.right = TreeNode(3)
-    println(isSameTree(first, second))
-    println(preorderTraversal(first))
 }
 
 class TreeNode(var `val`: Int) {
@@ -34,8 +32,111 @@ fun preorderTraversal(root: TreeNode?): List<Int> {
     return list
 }
 
-fun isSameTree(p: TreeNode?, q: TreeNode?): Boolean {
-    if (p == null && q == null) return true
-    if (p?.`val` != q?.`val`) return false
-    return  isSameTree(p?.left, q?.left) && isSameTree(p?.right, q?.right)
+fun stackPush(): List<Int> {
+    val stack = Stack<Int>()
+    stack.push(5)
+    stack.push(2)
+    stack.pop()
+    stack.push(5)
+    stack.push(0)
+    stack.pop()
+    stack.pop()
+    stack.pop()
+    stack.push(1)
+    return stack
+}
+
+fun minTime(n: Int, edges: List<List<Int>>, hasApple: BooleanArray): Int {
+    val visited = BooleanArray(n)
+    val graph = Array(n) { mutableListOf<Int>() }
+
+    edges.forEach { (s, d) ->
+        graph[s].add(d)
+        graph[d].add(s)
+    }
+
+    fun dfs(vertices: MutableList<Int>, i: Int): Int {
+        if (visited[i]) {
+            return 0
+        }
+        visited[i] = true
+
+        var total = 0
+        vertices.forEach { v ->
+            total += dfs(graph[v], v)
+        }
+        if (i != 0 && (hasApple[i] || total != 0)) {
+            total += 2
+        }
+        return total
+    }
+
+    return dfs(graph[0], 0)
+}
+
+private lateinit var result: IntArray
+fun countSubTrees(n: Int, edges: Array<IntArray>, labels: String): IntArray {
+    result = IntArray(n)
+    val graph: MutableMap<Int, MutableList<Int>> = mutableMapOf()
+    for (i in 0 until n) graph[i] = mutableListOf()
+    edges.forEach { edge ->
+        graph[edge[0]]!!.add(edge[1])
+        graph[edge[1]]!!.add(edge[0])
+    }
+    dfs(0, graph, BooleanArray(n), labels)
+    return result
+}
+
+fun dfs(current: Int, graph: Map<Int, List<Int>>, visited: BooleanArray, labels: String): IntArray {
+    // mark this node as visited.
+    visited[current] = true
+    // initialize child-label-counter.
+    val chars = IntArray(26)
+    // iterate childs.
+    graph[current]!!.forEach { child ->
+        if (!visited[child]) {
+            // if not visited, get child's accumulated label-count
+            val count = dfs(child, graph, visited, labels)
+            for (i in 0 until 26) chars[i] += count[i]
+        }
+    }
+    // add current word to label-counter and pass bottom-up.
+    result[current] = ++chars[labels[current] - 'a']
+    return chars
+}
+
+// roads = [[0,1],[0,2],[0,3]], seats = 5
+//  roads = [[3,1],[3,2],[1,0],[0,4],[0,5],[4,6]], seats = 2
+data class R(val cars: Long, val capacity: Int, val fuel: Long)
+fun minimumFuelCost(roads: Array<IntArray>, seats: Int): Long {
+    val nodes = mutableMapOf<Int, MutableList<Int>>()
+    roads.forEach { (from, to) ->
+        nodes.getOrPut(from, { mutableListOf() }) += to
+        nodes.getOrPut(to, { mutableListOf() }) += from
+    }
+    fun dfs(curr: Int, parent: Int): R {
+        val children = nodes[curr]
+        if (children == null) return R(1L, seats - 1, 0L)
+        var fuel = 0L
+        var capacity = 0
+        var cars = 0L
+        children.filter { it != parent }.forEach {
+            val r = dfs(it, curr)
+            fuel += r.cars + r.fuel
+            capacity += r.capacity
+            cars += r.cars
+        }
+        // seat this passenger
+        if (capacity == 0) {
+            cars++
+            capacity = seats - 1
+        } else capacity--
+        // optimize cars
+        while (capacity - seats >= 0) {
+            capacity -= seats
+            cars--
+        }
+        return R(cars, capacity, fuel)
+    }
+    return dfs(0, 0).fuel
 }
